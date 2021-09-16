f4ca0f56d53fd2c9137b19ee40c115525dc0f0ab
igors <igor.sutikov@ivkhk.ee>
2021-09-16 13:37:01 +0300

    Fir1

---
+++ src/sptv20_gitprob/Sptv20_GitProb.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptv20_gitprob;
+
+/**
+ *
+ * @author user
+ */
+public class Sptv20_GitProb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Fir1");
+        // TODO code application logic here
+    }
+    
+}
