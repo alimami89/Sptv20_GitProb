f1d66b08fc9f42890f930dfca7bd71fa8e963ec7
igors <igor.sutikov@ivkhk.ee>
2021-09-16 14:48:48 +0300

    second

--- src/sptv20_gitprob/Sptv20_GitProb.java
+++ src/sptv20_gitprob/Sptv20_GitProb.java
@@ -16,6 +16,7 @@ public class Sptv20_GitProb {
      */
     public static void main(String[] args) {
         System.out.println("Fir1");
+        System.out.println("second");
         // TODO code application logic here
     }
     
